d0e8ad5d5db4d63ef2e15227e958b0b86f6c1de5
Baton23 <92729911+Baton23@users.noreply.github.com>
2024-06-24 10:18:58 +0300

    Add files via upload
    TON based economical stratgy

---
+++ BusinessScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BusinessScreen = () => {
+    return (
+        <div className="business-screen">
+            <h1 className="title">Business Management</h1>
+            <p>Welcome to your new business!</p>
+            {/* Здесь вы можете добавить больше логики и компонентов для управления бизнесом */}
+        </div>
+    );
+};
+
+export default BusinessScreen;
---
+++ MainScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/main.css'; // Правильный путь к стилям
+
+const MainScreen = () => {
+    return (
+        <div className="main-screen">
+            <div className="logo">Russia-The90`s</div>
+            <button className="start-button">Start Game</button>
+        </div>
+    );
+}
+
+export default MainScreen;
